dbb3600e9a580a5db07a5367badb737504eaafec
TonyIommiSG <tonyiommisg>
2023-05-09 11:30:33 -0700

    fixing stuff

--- template_content/protocol/assets/voting_clear.py
+++
@@ -1,9 +0,0 @@
-from pyteal import *
-from helpers.voting_base import *
-
-def clear_state_program():
-    program = on_clear_logic()
-    return program
-
-if __name__ == "__main__":
-    print(compileTeal(clear_state_program(), Mode.Application, version = 8))
